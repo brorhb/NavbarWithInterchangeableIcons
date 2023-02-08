b61622be80cd746f872bd8f0595a54deaceec8fe
Bror Brurberg <bror.hammer.brurberg@eika.no>
2023-02-08 12:43:27 +0100

    Initial Commit

---
+++ CustomNavbar/CustomNavbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavbarApp.swift
+//  CustomNavbar
+//
+//  Created by Bror Brurberg on 08/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
